b8a82f9dc87f22eee153ad3ec971def0a005e12b
idvarnew <idvarnew@gmail.com>
2019-02-17 15:05:59 +0100

    Add mocks

---
+++ src/testing/post.ts
@@ -0,0 +1,24 @@
+export const newPost = {
+    title: 'Summer',
+    id: '_5my2l15b9',
+    publishDate: '2018-12-09T22:37:11.137Z',
+    description: { text: 'My holiday' },
+    labels: [],
+    img: {
+      small: [
+        {
+          name: 'size-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg',
+          url:
+            'https://firebasestorage.googleapis.com/v0/b/photos-93ec1.appspot.com/o/uploads%2Fsize-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg?alt=media&token=c0e26745-19c7-4c3b-a204-dbc9ef27748c'
+        }
+      ],
+      large: [
+        {
+          name: 'size-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg',
+          url:
+            'https://firebasestorage.googleapis.com/v0/b/photos-93ec1.appspot.com/o/uploads%2Fsize-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg?alt=media&token=c0e26745-19c7-4c3b-a204-dbc9ef27748c'
+        }
+      ]
+    }
+  };
+  
---
+++ src/testing/posts.ts
@@ -0,0 +1,37 @@
+export const posts = [
+    {
+      id: '_5my2l15b9',
+      title: 'title',
+      description: 'string',
+      publishDate: '2018-12-09T22:37:11.137Z',
+      labels: ['garden'],
+      comments: [],
+      likes: 0,
+      img: {
+        small: [
+          {
+            name: 'size-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg',
+            url:
+              'https://firebasestorage.googleapis.com/v0/b/photos-93ec1.appspot.com/o/uploads%2Fsize-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg?alt=media&token=c0e26745-19c7-4c3b-a204-dbc9ef27748c'
+          },
+          {
+            name: 'size-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg',
+            url:
+              'https://firebasestorage.googleapis.com/v0/b/photos-93ec1.appspot.com/o/uploads%2Fsize-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg?alt=media&token=c0e26745-19c7-4c3b-a204-dbc9ef27748c'
+          }
+        ],
+        large: [
+          {
+            name: 'size-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg',
+            url:
+              'https://firebasestorage.googleapis.com/v0/b/photos-93ec1.appspot.com/o/uploads%2Fsize-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg?alt=media&token=c0e26745-19c7-4c3b-a204-dbc9ef27748c'
+          },
+          {
+            name: 'size-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg',
+            url:
+              'https://firebasestorage.googleapis.com/v0/b/photos-93ec1.appspot.com/o/uploads%2Fsize-800-63jo8i2vyanita-austvika-1096223-unsplash.jpg?alt=media&token=c0e26745-19c7-4c3b-a204-dbc9ef27748c'
+          }
+        ]
+      }
+    }
+  ];
